fd46e15543d1a4b9c3fdac7b662e06536e2bd5e3
Muthia Aisyah Putri <108161059+muthiaap@users.noreply.github.com>
2025-07-18 15:52:31 +0700

    Add .next/static/chunks/src_components_MapComponent_tsx_0ce2c282._.js 1752828750548

---
+++ .next/static/chunks/src_components_MapComponent_tsx_0ce2c282._.js
@@ -0,0 +1,23 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/MapComponent.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_9b6918a9._.js",
+  "static/chunks/src_components_MapComponent_tsx_524c5f03._.js",
+  {
+    "path": "static/chunks/node_modules_leaflet_dist_leaflet_88e19fd3.css",
+    "included": [
+      "[project]/node_modules/leaflet/dist/leaflet.css [app-client] (css)"
+    ]
+  },
+  "static/chunks/src_components_MapComponent_tsx_d20649c0._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/MapComponent.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
