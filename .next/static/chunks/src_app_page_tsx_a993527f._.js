6ec6a510c56c1d9af7c0517307a1a3a5717dbd49
Muthia Aisyah Putri <108161059+muthiaap@users.noreply.github.com>
2025-07-18 15:52:37 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752828756351

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_MapComponent_tsx_0ce2c282._.js",
+  "static/chunks/node_modules_1c06d166._.js",
+  "static/chunks/src_379747fd._.js"
+],
+    source: "dynamic"
+});
